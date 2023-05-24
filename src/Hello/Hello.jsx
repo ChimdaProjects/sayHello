3c5c04e48a7ecf1de962368858d60deac489b814
Chimda B <chimdabrignoli@gmail.com>
2023-05-24 16:22:41 +0200

    create component Hello

---
+++ src/Hello/Hello.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export function Hello({name}) {
+  return (
+    <>
+        <h1>Hello {name} ! </h1>
+        <p>Quelle belle journée pour coder !</p>
+    </>
+  );
+}
+
+
+
